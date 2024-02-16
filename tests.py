7b17520eb734d472380809105832907ba6f93b49
bawejahritik <bawejahritik@gmail.com>
2024-02-16 04:08:41 -0500

    Add Unit Test for break_entrance_and_exit method

--- tests.py
+++ tests.py
@@ -28,6 +28,22 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+    
+    def test_maze_break_entrance_and_exit(self):
+        top_wall = False
+        bottom_wall = False
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            top_wall
+        )
+
+        self.assertEqual(
+            m1._cells[-1][-1].has_bottom_wall,
+            bottom_wall
+        )
 
 
 if __name__ == "__main__":
